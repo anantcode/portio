4157173d12a6aaa8c8aedc1377a4bdfcdee4b224
anantpan <anantpan@ANANTPAN-IN.oradev.oraclecorp.com>
2020-07-15 19:17:53 +0530

    Updates

---
+++ precache-manifest.01b7d2cd3831e5faeb8e65e437e41f7c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08dca987672464d40e75bbb44eea75ab",
+    "url": "/porto/index.html"
+  },
+  {
+    "revision": "eb22f6dcddd5f1f390cd",
+    "url": "/porto/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "245fe8f454dc18da92bf",
+    "url": "/porto/static/css/main.b9b614ff.chunk.css"
+  },
+  {
+    "revision": "eb22f6dcddd5f1f390cd",
+    "url": "/porto/static/js/2.4044ce23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/porto/static/js/2.4044ce23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "245fe8f454dc18da92bf",
+    "url": "/porto/static/js/main.31e715fe.chunk.js"
+  },
+  {
+    "revision": "56ee7bec2d05e38ebb38",
+    "url": "/porto/static/js/runtime-main.a211ec4b.js"
+  },
+  {
+    "revision": "74d8aea791405f14cb04a18f0b93120a",
+    "url": "/porto/static/media/BallpointSignature-L3J9W.74d8aea7.woff"
+  }
+]);
